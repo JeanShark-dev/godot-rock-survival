ef559cff882f232a4406ff28f5f22c1a2f68c1cc
Jean Shark <84136135+JeanShark-dev@users.noreply.github.com>
2022-11-08 13:02:41 +0200

    basic movement and input
    added: player wiþ wasd 8-way movement and a boulder

---
+++ PlayerMovementBase.gd
@@ -0,0 +1,22 @@
+extends KinematicBody2D
+
+export (float) var baseMoveSpeed
+var moveVelocity = Vector2()
+
+func GetMoveInput():
+	moveVelocity = Vector2()
+	if (Input.is_action_pressed("gp_up")):
+		moveVelocity.y -= baseMoveSpeed
+	if (Input.is_action_pressed("gp_down")):
+		moveVelocity.y += baseMoveSpeed
+	if (Input.is_action_pressed("gp_left")):
+		moveVelocity.x -= baseMoveSpeed
+	if (Input.is_action_pressed("gp_right")):
+		moveVelocity.x += baseMoveSpeed
+	moveVelocity = moveVelocity.normalized() * baseMoveSpeed
+
+func _process(delta):
+	GetMoveInput()
+
+func _physics_process(delta):
+	move_and_slide(moveVelocity)
