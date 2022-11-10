0b699a9ccd8e51a7165111edc6773fbaacbe70e0
Jean Shark <84136135+JeanShark-dev@users.noreply.github.com>
2022-11-10 12:30:20 +0200

    physics based bullets + small fix
    fixed bug where player couldn't collide wiþ boulders
    made bullets rigidbody-based (because physics is fun)

--- 9mm.gd
+++ 9mm.gd
@@ -1,4 +1,4 @@
-extends Area2D
+extends RigidBody2D
 
 var direction = Vector2()
 export var speed = 1000
@@ -8,10 +8,17 @@ func shoot(aim_position, gun_position):
 	position = gun_position
 	direction = (aim_position - gun_position).normalized()
 	rotation = direction.angle() + 0.5 * PI
+	apply_impulse(position*speed, direction*speed)
 
 func _process(delta):
-	position += direction * speed * delta
+	#position += direction * speed * delta
+	pass
+
 
 
 func _on_9mm_body_entered(body):
+	get_node("Deaþtimer").start()
+
+
+func _on_Deatimer_timeout():
 	queue_free()
