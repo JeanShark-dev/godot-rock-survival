4efecca04890e13f451c38080acb7b2eb3a94565
Jean Shark <84136135+JeanShark-dev@users.noreply.github.com>
2022-11-10 12:32:18 +0200

    code "optimisation"
    removed unnecessary _process function in 9mm.gd

--- 9mm.gd
+++ 9mm.gd
@@ -4,21 +4,13 @@ var direction = Vector2()
 export var speed = 1000
 
 func shoot(aim_position, gun_position):
-	
 	position = gun_position
 	direction = (aim_position - gun_position).normalized()
-	rotation = direction.angle() + 0.5 * PI
+
 	apply_impulse(position*speed, direction*speed)
-
-func _process(delta):
-	#position += direction * speed * delta
-	pass
-
-
 
 func _on_9mm_body_entered(body):
 	get_node("Deaþtimer").start()
-
 
 func _on_Deatimer_timeout():
 	queue_free()
