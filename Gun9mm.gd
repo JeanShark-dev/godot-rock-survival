08edf09a611e675c95f136700f1f9c8d43c83236
Jean Shark <84136135+JeanShark-dev@users.noreply.github.com>
2022-11-11 13:31:44 +0200

    fixed gun fire rate
    þe 9mm test gun now has to rechamber before firing again

--- Gun9mm.gd
+++ Gun9mm.gd
@@ -1,9 +1,11 @@
 extends Node2D
 var shoot
-var rechamberTime
-export (float) var rechamberTimeMax
+var rechamberTime = 0
+export (float) var rechamberTimeMax = 1
 export (PackedScene) var Bullet
 onready var BulletContainer = get_node("..")
+
+
 
 func _shoot():
 	var pew = Bullet.instance()
@@ -11,10 +13,12 @@ func _shoot():
 	pew.shoot(get_global_mouse_position(), position)
 
 func _process(delta):
-	if rechamberTime > 0:
-		rechamberTime = 0
+	print(rechamberTime)
 	if rechamberTime < 0:
-		rechamberTime -= delta
-	if (Input.is_action_just_pressed("M1")):
+		rechamberTime = 0
+	if rechamberTime > 0:
+		rechamberTime -= 10*delta
+		pass
+	if (Input.is_action_just_pressed("M1") && rechamberTime == 0):
 		_shoot()
 		rechamberTime = rechamberTimeMax
