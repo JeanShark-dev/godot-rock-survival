ac7694e51304b4f7c272a06a960b96c12b41c3d5
Jean Shark <84136135+JeanShark-dev@users.noreply.github.com>
2023-07-11 14:18:58 +0300

    pause menu now pauses þe game

--- GUI.gd
+++ GUI.gd
@@ -2,6 +2,7 @@ extends Control
 
 
 var mainNode
+
 func _ready():
 	mainNode = get_parent().get_parent().get_parent().get_parent()
 
@@ -10,10 +11,12 @@ func _ready():
 func _process(delta):
 	if Input.is_action_just_pressed("ui_cancel"):
 		$PauseMenu.visible = !$PauseMenu.visible
+		get_tree().paused = !get_tree().paused
 
 
 func _on_resume_button_pressed():
 	$PauseMenu.visible = !$PauseMenu.visible
+	get_tree().paused = !get_tree().paused
 
 
 func _on_quit_button_pressed():
