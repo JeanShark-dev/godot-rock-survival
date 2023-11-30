163be27f7d01fb137f39a604072d8276b28ff2f0
Jean Shark <84136135+JeanShark-dev@users.noreply.github.com>
2023-11-30 12:20:27 +0200

    moved main menu loading to script

--- assets/scripts/main.gd
+++ assets/scripts/main.gd
@@ -1,2 +1,17 @@
 extends Node
 
+func _ready():
+	load_main_menu()
+	pass
+
+
+func load_main_menu():
+	print("Loading Main Menu...")
+	if get_node("Main Menu") != null:
+		print("Existing Main Menu found, removing...")
+		$"Main Menu".queue_free()
+	var main_menu = load("res://assets/scenes/main_menu.tscn")
+	var instance = main_menu.instantiate()
+	add_child(instance)
+	print("Main Menu done.")
+	
