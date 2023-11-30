77fe80bc48575020da408bd4c3bd6d67334e328c
Jean Shark <84136135+JeanShark-dev@users.noreply.github.com>
2023-11-30 13:33:05 +0200

    primitive player movement
    added a player char'body2d,
    yoinked movement script from þe godot docs,
    and removed gravity

---
+++ assets/scripts/player.gd
@@ -0,0 +1,14 @@
+extends CharacterBody2D
+
+
+var movement_speed_base = 100
+
+
+func get_input():
+	var input_direction = Input.get_vector("mv_left", "mv_right", "mv_up", "mv_down")
+	velocity = input_direction * movement_speed_base
+
+
+func _physics_process(delta):
+	get_input()
+	move_and_slide()
